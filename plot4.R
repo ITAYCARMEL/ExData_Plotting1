afa7ef98b4b3dc1bb5841c78cbb6136b655f2de8
ITAY CARMEL <itay@minols.com>
2018-09-26 11:41:45 +0300

    "Added h1 tag"

---
+++ plot4.R
@@ -0,0 +1,22 @@
+#plot4
+library(tidyr)
+library(dplyr)
+library(lubridate)
+setwd("~/R")
+plot4names<-read.table("household_power_consumption.txt",header=FALSE,nrows=1,sep=";",na.strings="?",stringsAsFactors = FALSE)
+plot4data<-read.table("household_power_consumption.txt",header=FALSE,skip=66637,nrows=2880,sep=";")
+colnames(plot4data)<-plot4names
+# convert the Date and Time variables to Date/Time classes in R using the strptime()  and as.Date() functions.
+plot4data<-unite(plot4data,"Date_Time",Date,Time,sep=" ",remove = TRUE)
+plot4data$Date_Time<-strptime(plot4data$Date_Time,"%d/%m/%Y %H:%M:%S")
+png("plot4.png",width = 480,height=480)
+par(mfrow=c(2,2))
+par(mar=c(6,4.2,5.2,2.1))
+with(plot4data,plot(x=Date_Time,y=Global_active_power,type="l",cex.lab=0.7,xlab="",ylab="Global active power",cex.axis=0.7))
+with(plot4data,plot(x=Date_Time,y=Voltage,type="l",cex.lab=0.7,xlab="datetime",ylab="Voltage",cex.axis=0.7))
+with(plot4data,plot(x=Date_Time,y=Sub_metering_1,type="l",xlab="",ylab="Energy sub metering",cex.lab=0.7,cex.axis=0.7))
+with(plot4data,lines(Date_Time,Sub_metering_2,col="red"))
+with(plot4data,lines(Date_Time,Sub_metering_3,col="blue"))
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"),lty=1,bty="n",cex=0.8)
+with(plot4data,plot(x=Date_Time,y=Global_reactive_power,type="l",cex.lab=0.7,xlab="datetime",cex.axis=0.7))
+dev.off()
